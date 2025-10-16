b595887baddd4a2ec15508a9af93157b363d00b9
fergordo994 <fer.gordo94+github@gmail.com>
2025-10-16 16:12:57 +0200

    Update transform.sql

--- sql/transform.sql
+++ sql/transform.sql
@@ -1,17 +1,25 @@
-MERGE `core-veld-475310-u0.INTEGRATION.integration_prueba_tecnica` T
+CREATE TABLE IF NOT EXISTS `INTEGRATION.integration_prueba_tecnica` (
+    PriceArea STRING,
+    HourUTC TIMESTAMP,
+    SpotPriceDKK FLOAT64,
+    SpotPriceEUR FLOAT64,
+    transform_date DATE
+);
+
+MERGE `INTEGRATION.integration_prueba_tecnica` T
 USING (
-  SELECT
-    PriceArea,
-    HourUTC,
-    SpotPriceDKK,
-    SpotPriceEUR,
-    CURRENT_DATE() AS transform_date
-  FROM (
-    SELECT *,
-      ROW_NUMBER() OVER (PARTITION BY PriceArea, HourUTC ORDER BY ingest_timestamp DESC) AS rn
-    FROM `core-veld-475310-u0.SANDBOX_apidata.elspot_prices`
-  )
-  WHERE rn = 1
+    SELECT
+        PriceArea,
+        TIMESTAMP(HourUTC) AS HourUTC,
+        SpotPriceDKK,
+        SpotPriceEUR,
+        CURRENT_DATE() AS transform_date
+    FROM (
+        SELECT *,
+            ROW_NUMBER() OVER (PARTITION BY PriceArea, HourUTC ORDER BY ingest_timestamp DESC) AS rn
+        FROM `core-veld-475310-u0.SANDBOX_apidata.elspot_prices`
+    )
+    WHERE rn = 1
 ) S
 ON T.PriceArea = S.PriceArea AND T.HourUTC = S.HourUTC
 WHEN MATCHED THEN
@@ -22,3 +30,5 @@ WHEN MATCHED THEN
 WHEN NOT MATCHED THEN
   INSERT (PriceArea, HourUTC, SpotPriceDKK, SpotPriceEUR, transform_date)
   VALUES (S.PriceArea, S.HourUTC, S.SpotPriceDKK, S.SpotPriceEUR, S.transform_date);
+
+
